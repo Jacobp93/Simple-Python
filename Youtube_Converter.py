ff742e1970dc71cb10ad5f5afe39728484805bbb
Jacobp93 <59118524+Jacobp93@users.noreply.github.com>
2022-03-30 20:53:32 +0100

    Add files via upload

---
+++ Youtube_Converter.py
@@ -0,0 +1,28 @@
+import youtube_dl
+import subprocess
+
+
+def run():
+    video_url = input("Please enter Youtube URL")
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url=video_url, download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+    options = {
+        'format': 'bestaudio/best',
+        'keepvideo': True,
+        'outtmpl': filename,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+    }
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    subprocess.call(["open, filename"])
+
+
+if __name__ == '__main__':
+    run()
